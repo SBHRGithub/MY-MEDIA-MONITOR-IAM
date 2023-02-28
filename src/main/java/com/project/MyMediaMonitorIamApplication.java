fd7eaaa578a8965a045046caffd3b81da8754d01
SBHR <sbhr2@hotmail.com>
2023-02-28 15:39:45 +0100

    Initial commit

---
+++ src/main/java/com/project/MyMediaMonitorIamApplication.java
@@ -0,0 +1,13 @@
+package com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMediaMonitorIamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMediaMonitorIamApplication.class, args);
+	}
+
+}
